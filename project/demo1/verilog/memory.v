50187c4b466f221a7b897b5bab15be5c46f29f4e
Kieran Mulligan <kmulligan@rockhopper-08.cs.wisc.edu>
2020-03-06 18:21:49 -0600

    memory

--- project/demo1/verilog/memory.v
+++ project/demo1/verilog/memory.v
@@ -5,8 +5,19 @@
    Description     : This module contains all components in the Memory stage of the 
                      processor.
 */
-module memory (/* TODO: Add appropriate inputs/outputs for your memory stage here*/);
-
+module memory (data_out, data_in, addr, en, wr, createdump, clk, rst);
+    output [15:0] data_out;
+    input [15:0] data_in;
+    input [15:0] addr;
+    input en;
+    input wr;
+    input createdump;
+    input clk;
+    input rst;
+    
    // TODO: Your code here
+   memory2c mem(.data_out(data_out), .data_in(data_in), .addr(addr), .enable(en), 
+        .wr(wr), .createdump(createdump), .clk(clk), .rst(rst));
    
+
 endmodule
