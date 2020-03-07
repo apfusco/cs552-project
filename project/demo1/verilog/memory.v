1308b97c0c358263fc699c338db3572ea05bfdc0
Kieran Mulligan <kmulligan@rockhopper-08.cs.wisc.edu>
2020-03-06 18:45:00 -0600

    Added memory module and branch mux to memory stage

--- project/demo1/verilog/memory.v
+++ project/demo1/verilog/memory.v
@@ -5,19 +5,32 @@
    Description     : This module contains all components in the Memory stage of the 
                      processor.
 */
-module memory (data_out, data_in, addr, en, wr, createdump, clk, rst);
+module memory (data_out, data_in, addr, en, wr, createdump, clk, rst, 
+        br, p, eq, n, zero, alu_out_msb);
+
+    // memory signals
     output [15:0] data_out;
     input [15:0] data_in;
     input [15:0] addr;
     input en;
-    input wr;
+    input mem_wr;
     input createdump;
     input clk;
     input rst;
+
+    // branching logic signals
+    output br;
+    input p;
+    input eq;
+    input n;
+    input zero; 
+    input alu_out_msb;
     
    // TODO: Your code here
    memory2c mem(.data_out(data_out), .data_in(data_in), .addr(addr), .enable(en), 
-        .wr(wr), .createdump(createdump), .clk(clk), .rst(rst));
+        .wr(mem_wr), .createdump(createdump), .clk(clk), .rst(rst));
    
+   // determines branching behavior based on result flags
+   mux4_1 mux(.InA(p), .InB(eq), .InC(n), .InD(1'bx), .S({alu_out_msb, zero}), .Out(br));
 
 endmodule
