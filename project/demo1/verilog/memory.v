d7038646ca61413c9e905fee9acca8756a4c9c4f
Alexander Fusco <afusco@wisc.edu>
2020-03-13 18:37:12 -0500

    memory: Replace branching logic with set logic

--- project/demo1/verilog/memory.v
+++ project/demo1/verilog/memory.v
@@ -5,8 +5,8 @@
    Description     : This module contains all components in the Memory stage of the 
                      processor.
 */
-module memory (data_out, data_in, addr, en, wr, createdump, clk, rst, 
-        br, p, eq, n, zero, alu_out_msb);
+module memory (data_out, data_in, addr, en, mem_wr, createdump, clk, rst,
+               set, ofl, zero, ltz, lteq, set_sel);
 
     // memory signals
     output [15:0] data_out;
@@ -18,19 +18,19 @@ module memory (data_out, data_in, addr, en, wr, createdump, clk, rst,
     input clk;
     input rst;
 
-    // branching logic signals
-    output br;
-    input p;
-    input eq;
-    input n;
-    input zero; 
-    input alu_out_msb;
+    // set logic signals
+    output      set;
+    input       ofl;
+    input       zero;
+    input       ltz;
+    input       lteq;
+    input [1:0] set_sel;
     
    // TODO: Your code here
    memory2c mem(.data_out(data_out), .data_in(data_in), .addr(addr), .enable(en), 
         .wr(mem_wr), .createdump(createdump), .clk(clk), .rst(rst));
    
    // determines branching behavior based on result flags
-   mux4_1 mux(.InA(p), .InB(eq), .InC(n), .InD(1'bx), .S({alu_out_msb, zero}), .Out(br));
+   mux4_1 mux(.InA(ofl), .InB(zero), .InC(ltz), .InD(lteq), .S(set_sel), .Out(set));
 
 endmodule
