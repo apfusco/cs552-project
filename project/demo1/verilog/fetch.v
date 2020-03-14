c9d8ac77ee2ed0806edd6d93bb0e1579514a2f17
Alexander Fusco <afusco@wisc.edu>
2020-03-14 12:18:22 -0500

    Debug progress

--- project/demo1/verilog/fetch.v
+++ project/demo1/verilog/fetch.v
@@ -4,10 +4,11 @@
    Filename        : fetch.v
    Description     : This is the module for the overall fetch stage of the processor.
 */
-module fetch (instr, PC_sext_imm, reg_sext_imm, clk, rst,
+module fetch (instr, PC_inc, PC_sext_imm, reg_sext_imm, clk, rst,
         mem_wr, dump, take_br, pc_en, jmp_reg_instr);
     
     output [15:0] instr;
+    output [15:0] PC_inc; // PC + 2
     input [15:0] PC_sext_imm; // PC + 2 + some immediate
     input [15:0] reg_sext_imm; // immediate from a register (ex. JR instruction)
     input clk;
@@ -21,7 +22,7 @@ module fetch (instr, PC_sext_imm, reg_sext_imm, clk, rst,
     wire mem_en;
     wire [15:0] nxt_PC;
     wire [15:0] PC_reg_out;
-    wire [15:0] PC_inc; // PC + 2
+    wire [15:0] PC_inc_wire; // PC + 2
     wire [15:0] two;
     wire [15:0] PC_mux_out;
     wire gnd;
@@ -29,13 +30,14 @@ module fetch (instr, PC_sext_imm, reg_sext_imm, clk, rst,
     assign gnd = 1'b0;
     assign mem_en = 1'b1;
     assign two = 16'h0002;
+    assign PC_inc = PC_inc_wire;
 
     // PC + 2 since our ISA uses 16 bit instructions
-    cla_16b pc_addr(.A(PC_reg_out), .B(two), .C_in(1'b0), .S(PC_inc), .C_out());
+    cla_16b pc_addr(.A(PC_reg_out), .B(two), .C_in(1'b0), .S(PC_inc_wire), .C_out());
 
     // select between PC + 2, a sign-extended imm, or an imm from a reg
     // assign PC_sel = (br_instr & take_br) | jump_instr;
-    mux2_1 PC_mux [15:0](.InA(PC_inc), .InB(PC_sext_imm), .S(take_br), 
+    mux2_1 PC_mux [15:0](.InA(PC_inc_wire), .InB(PC_sext_imm), .S(take_br), 
             .Out(PC_mux_out));
     mux2_1 jump_reg_mux [15:0](.InA(PC_mux_out), .InB(reg_sext_imm),
             .S(jmp_reg_instr), .Out(nxt_PC));
