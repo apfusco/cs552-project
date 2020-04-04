7259e6fc13d976134c2b0cbb406a13c45812519a
Kieran Mulligan <kmulligan@royal-12.cs.wisc.edu>
2020-04-03 21:27:58 -0500

    Added stall bubble ports to pipeline stages

--- project/demo2/verilog/mem_wb.v
+++ project/demo2/verilog/mem_wb.v
@@ -23,7 +23,8 @@ module mem_wb(
         in_LBI,
         in_SLBI,
         in_sext_imm,
-        in_set);
+        in_set,
+        in_stall_n);
 
    output        out_wr_en;
    output [2:0]  out_wr_reg;
@@ -49,18 +50,19 @@ module mem_wb(
    input [15:0] in_SLBI;
    input [15:0] in_sext_imm;
    input        in_set;
+   input        in_stall_n; // low if stage should stall
 
    // TODO: writeEn needs to be low in the event of a stall.
    // TODO: wr_en needs to be low in the event of a stall.
-   register #(.N(1)) wr_en_reg(.clk(clk), .rst(rst), .writeEn(1'b1), .dataIn(in_wr_en), .dataOut(out_wr_en), .err());
-   register #(.N(3)) wr_reg_reg(.clk(clk), .rst(rst), .writeEn(1'b1), .dataIn(in_wr_reg), .dataOut(out_wr_reg), .err());
-   register #(.N(3)) wr_sel_reg(.clk(clk), .rst(rst), .writeEn(1'b1), .dataIn(in_wr_sel), .dataOut(out_wr_sel), .err());
-   register #(.N(16)) alu_out_reg(.clk(clk), .rst(rst), .writeEn(1'b1), .dataIn(in_alu_out), .dataOut(out_alu_out), .err());
-   register #(.N(16)) mem_out_reg(.clk(clk), .rst(rst), .writeEn(1'b1), .dataIn(in_mem_out), .dataOut(out_mem_out), .err());
-   register #(.N(16)) PC_inc_reg(.clk(clk), .rst(rst), .writeEn(1'b1), .dataIn(in_PC_inc), .dataOut(out_PC_inc), .err());
-   register #(.N(16)) LBI_reg(.clk(clk), .rst(rst), .writeEn(1'b1), .dataIn(in_LBI), .dataOut(out_LBI), .err());
-   register #(.N(16)) SLBI_reg(.clk(clk), .rst(rst), .writeEn(1'b1), .dataIn(in_SLBI), .dataOut(out_SLBI), .err());
-   register #(.N(16)) sext_imm_reg(.clk(clk), .rst(rst), .writeEn(1'b1), .dataIn(in_sext_imm), .dataOut(out_sext_imm), .err());
-   register #(.N(1)) set_reg(.clk(clk), .rst(rst), .writeEn(1'b1), .dataIn(in_set), .dataOut(out_set), .err());
+   register #(.N(1)) wr_en_reg(.clk(clk), .rst(rst), .writeEn(in_stall_n), .dataIn(in_wr_en), .dataOut(out_wr_en), .err());
+   register #(.N(3)) wr_reg_reg(.clk(clk), .rst(rst), .writeEn(in_stall_n), .dataIn(in_wr_reg), .dataOut(out_wr_reg), .err());
+   register #(.N(3)) wr_sel_reg(.clk(clk), .rst(rst), .writeEn(in_stall_n), .dataIn(in_wr_sel), .dataOut(out_wr_sel), .err());
+   register #(.N(16)) alu_out_reg(.clk(clk), .rst(rst), .writeEn(in_stall_n), .dataIn(in_alu_out), .dataOut(out_alu_out), .err());
+   register #(.N(16)) mem_out_reg(.clk(clk), .rst(rst), .writeEn(in_stall_n), .dataIn(in_mem_out), .dataOut(out_mem_out), .err());
+   register #(.N(16)) PC_inc_reg(.clk(clk), .rst(rst), .writeEn(in_stall_n), .dataIn(in_PC_inc), .dataOut(out_PC_inc), .err());
+   register #(.N(16)) LBI_reg(.clk(clk), .rst(rst), .writeEn(in_stall_n), .dataIn(in_LBI), .dataOut(out_LBI), .err());
+   register #(.N(16)) SLBI_reg(.clk(clk), .rst(rst), .writeEn(in_stall_n), .dataIn(in_SLBI), .dataOut(out_SLBI), .err());
+   register #(.N(16)) sext_imm_reg(.clk(clk), .rst(rst), .writeEn(in_stall_n), .dataIn(in_sext_imm), .dataOut(out_sext_imm), .err());
+   register #(.N(1)) set_reg(.clk(clk), .rst(rst), .writeEn(in_stall_n), .dataIn(in_set), .dataOut(out_set), .err());
 
 endmodule
