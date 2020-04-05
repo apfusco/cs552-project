209fca5954c92e4f225e9d40a37ada591ab63460
Kieran Mulligan <kmulligan@royal-23.cs.wisc.edu>
2020-04-05 01:14:06 -0500

    Added mem to mem forwarding for store instructions

--- project/demo2/verilog/forward.v
+++ project/demo2/verilog/forward.v
@@ -11,6 +11,8 @@ module forward(
         ex_Rt,
         mem_Rs,
         mem_Rt,
+        mem_to_mem_Rs,
+        mem_to_mem_fwd_Rs,
         // inputs
         stall,
         mem_wr_en,
@@ -18,6 +20,8 @@ module forward(
         id_ex_has_Rt,
         id_ex_Rs, 
         id_ex_Rt,
+        ex_mem_Rs,
+        id_ex_mem_wr, // id_mem_wr signal
         wb_wr_en,
         mem_wb_Rd,
         ex_mem_alu_result,
@@ -30,18 +34,21 @@ module forward(
         mem_wb_set_result,
         mem_wb_lbi_result,
         mem_wb_slbi_result,
-        mem_wb_wr_sel
-        );
+        mem_wb_wr_sel,
+        mem_wb_mem_wr);
+
 // TODO: make set_inputs be 1 bit and then add zeros
 
     output        ex_fwd_Rs; // forward Rs to ex from ex
     output        ex_fwd_Rt; // forward Rt to ex from ex
     output        mem_fwd_Rs; // forward Rs to ex from mem
     output        mem_fwd_Rt; // forward Rt to ex from mem
+    output        mem_to_mem_fwd_Rs; // forward Rs to mem from mem
     output [15:0] ex_Rs; // actual values forwarded
     output [15:0] ex_Rt;
     output [15:0] mem_Rs;
     output [15:0] mem_Rt;
+    output [15:0] mem_to_mem_Rs;
     
     input        stall;
     input        mem_wr_en; // ex/mem stage reg write signal
@@ -49,6 +56,8 @@ module forward(
     input        id_ex_has_Rt;
     input [2:0]  id_ex_Rs;
     input [2:0]  id_ex_Rt;
+    input [2:0]  ex_mem_Rs;
+    input        id_ex_mem_wr;
     input        wb_wr_en; // mem/wb stage reg write signal
     input [2:0]  mem_wb_Rd;
     input [15:0] ex_mem_alu_result; // ex stage results
@@ -62,6 +71,7 @@ module forward(
     input [15:0] mem_wb_lbi_result;
     input [15:0] mem_wb_slbi_result;
     input [2:0]  mem_wb_wr_sel;
+    input        mem_wb_mem_wr;
     // TODO: PC_inc results needed or no?
 
     wire ex_mem_reg_wr;// TODO: This signal isn't used...?
@@ -75,13 +85,14 @@ module forward(
     assign ex_fwd_Rs = mem_wr_en & ~|(ex_mem_Rd ^ id_ex_Rs);
     assign ex_fwd_Rt = mem_wr_en & id_ex_has_Rt & ~|(ex_mem_Rd ^ id_ex_Rt);
 
-
     // TODO: may need to manually implement !(e/m_wr_en & r/m_rd != d/e_rt)
 
     // foward in MEM if writing result to reg and the MEM/WB dest and ID/EX
     // source registers are the same, AND there is no prior EX forwarding
     assign mem_fwd_Rs = wb_wr_en & ~|(mem_wb_Rd ^ id_ex_Rs) & ~ex_fwd_Rs;
     assign mem_fwd_Rt = wb_wr_en & id_ex_has_Rt & ~|(mem_wb_Rd ^ id_ex_Rt) & ~ex_fwd_Rt;
+
+    assign mem_to_mem_fwd_Rs = mem_wb_mem_wr & ~|(mem_wb_Rd ^ ex_mem_Rs);
 
     // hard to compress into a 4:1 mux because of how wr_sel is set up
         // could feasibly just make this a one-hot-esque thing
@@ -120,4 +131,6 @@ module forward(
     assign mem_Rs = mem_fwd_Rs ? mem_wr_data : 16'h0000;
     assign mem_Rt = mem_fwd_Rt ? mem_wr_data : 16'h0000;
 
+    // TODO: can I replace the true val with just an alu_result...
+    assign mem_to_mem_Rs = mem_to_mem_fwd_Rs ? mem_wr_data : 16'h0000;
 endmodule
