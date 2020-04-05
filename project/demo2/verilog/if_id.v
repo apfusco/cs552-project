dff30d742351dee501428d362bb579e0f1b21014
Alexander Fusco <afusco@wisc.edu>
2020-04-04 19:35:21 -0500

    if_id: Add handling for flushing instruction

--- project/demo2/verilog/if_id.v
+++ project/demo2/verilog/if_id.v
@@ -10,6 +10,7 @@ module if_id(
         in_instr,
         in_stall_n,
         in_PC_inc,
+        take_new_PC,
         in_halt);
 
    output [15:0] out_instr;
@@ -22,13 +23,17 @@ module if_id(
    input [15:0] in_instr;
    input [15:0] in_PC_inc;
    input        in_stall_n; // active low
+   input        take_new_PC;
    input        in_halt;
 
    assign err = (^{clk, rst, in_instr, in_stall_n, in_PC_inc, in_halt} === 1'bX) ? 1'b1
          : 1'b0;
 
+   wire [15:0] instr;
+   assign instr = take_new_PC : {5'b00001, in_instr[10:0]} ? in_instr;
+
     // TODO: add writeEn signal once fowarding logic is implemented for stalls
-   register #(.N(16)) instr_reg(.clk(clk), .rst(rst), .writeEn(in_stall_n), .dataIn(in_instr), .dataOut(out_instr), .err());
+   register #(.N(16)) instr_reg(.clk(clk), .rst(rst), .writeEn(in_stall_n), .dataIn(instr), .dataOut(out_instr), .err());
    register #(.N(16)) PC_inc_reg(.clk(clk), .rst(rst), .writeEn(in_stall_n), .dataIn(in_PC_inc), .dataOut(out_PC_inc), .err());
    register #(.N(1)) halt_reg(.clk(clk), .rst(rst), .writeEn(in_stall_n), .dataIn(in_halt), .dataOut(out_halt), .err());
 
